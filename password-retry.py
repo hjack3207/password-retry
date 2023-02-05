a327599eee03ee61d906f06cbbda3c87c76db355
hjack3207 <hjack3207@gmail.com>
2023-02-05 17:53:56 +0800

    change to while i >0

--- password-retry.py
+++ password-retry.py
@@ -1,15 +1,10 @@
 password = 'a123456'
-time = 3
-while True:
-	if time == 0:
-		print('失敗太多次')
+time = 3 # 剩餘機會
+while time > 0:	
+	password_try = input('請輸入密碼')
+	if password_try == password:
+		print('登入成功')
 		break
-	else:	
-		password_try = input('請輸入密碼')
-		if password_try == password:
-			print('登入成功')
-			break
-		else:
-			print('密碼錯誤，還有', time - 1,'次機會')
-			time = time - 1	
-
+	else:
+		time = time - 1
+		print('密碼錯誤，還有', time,'次機會')
